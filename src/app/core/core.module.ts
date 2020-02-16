8f0fc35355959959aa1a20c8b6ae9ba302af2656
Mohammad sharifi <m.sharifi@SEPIDZ.NET>
2020-02-16 15:29:17 +0330

    chore(core): updagrade to Angular v9

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -1,22 +1,13 @@
-import {
-  MatButtonModule,
-  MatIconModule,
-  MatListModule,
-  MatSidenavModule,
-  MatTabsModule,
-  MatToolbarModule
-} from '@angular/material';
-import {
-  NgModule,
-  NgModuleFactoryLoader,
-  Optional,
-  SkipSelf,
-  SystemJsNgModuleLoader
-} from '@angular/core';
-
-import { CommonModule } from '@angular/common';
-import { LayoutModule } from '@angular/cdk/layout';
-import { TabsComponent } from './components/tabs/tabs.component';
+import { NgModule, Optional, SkipSelf } from "@angular/core";
+import { MatTabsModule } from "@angular/material/tabs";
+import { MatListModule } from "@angular/material/list";
+import { MatIconModule } from "@angular/material/icon";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatButtonModule } from "@angular/material/button";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { LayoutModule } from "@angular/cdk/layout";
+import { CommonModule } from "@angular/common";
+import { TabsComponent } from "./components/tabs/tabs.component";
 
 @NgModule({
   imports: [
@@ -44,13 +35,12 @@ import { TabsComponent } from './components/tabs/tabs.component';
        This new feature allows cleaning up the providers section from the CoreModule.
        But if you want to provide something with an InjectionToken other that its class, you still have to use this section.
     */
-    { provide: NgModuleFactoryLoader, useClass: SystemJsNgModuleLoader }
   ]
 })
 export class CoreModule {
   constructor(@Optional() @SkipSelf() core: CoreModule) {
     if (core) {
-      throw new Error('CoreModule should be imported ONLY in AppModule.');
+      throw new Error("CoreModule should be imported ONLY in AppModule.");
     }
   }
 }
--- src/app/shared/material.module.ts
+++ src/app/shared/material.module.ts
@@ -1,45 +1,52 @@
 import {
-  MatAutocompleteModule,
-  MatButtonModule,
-  MatButtonToggleModule,
-  MatCardModule,
-  MatCheckboxModule,
-  MatChipsModule,
-  MatDatepickerModule,
-  MatDialogModule,
-  MatExpansionModule,
-  MatFormFieldModule,
-  MatGridListModule,
-  MatIconModule,
-  MatInputModule,
-  MatListModule,
-  MatMenuModule,
-  MatNativeDateModule,
-  MatPaginatorModule,
-  MatProgressBarModule,
-  MatProgressSpinnerModule,
-  MatRadioModule,
-  MatRippleModule,
-  MatSelectModule,
-  MatSidenavModule,
-  MatSlideToggleModule,
-  MatSliderModule,
-  MatSnackBarModule,
-  MatSortModule,
-  MatStepperModule,
-  MatTableModule,
-  MatTabsModule,
-  MatToolbarModule,
-  MatTooltipModule,
-} from '@angular/material';
+  DateAdapter,
+  MAT_DATE_FORMATS,
+  MAT_DATE_LOCALE
+} from "@angular/material/core";
 
-import { CdkTableModule } from '@angular/cdk/table';
-import { NgModule } from '@angular/core';
+import { CdkTableModule } from "@angular/cdk/table";
+import { MatFormFieldModule } from "@angular/material/form-field";
+import { MatAutocompleteModule } from "@angular/material/autocomplete";
+import { MatBadgeModule } from "@angular/material/badge";
+import { MatBottomSheetModule } from "@angular/material/bottom-sheet";
+import { MatButtonModule } from "@angular/material/button";
+import { MatButtonToggleModule } from "@angular/material/button-toggle";
+import { MatCardModule } from "@angular/material/card";
+import { MatCheckboxModule } from "@angular/material/checkbox";
+import { MatChipsModule } from "@angular/material/chips";
+import { MatStepperModule } from "@angular/material/stepper";
+import { MatDatepickerModule } from "@angular/material/datepicker";
+import { MatDialogModule } from "@angular/material/dialog";
+import { MatExpansionModule } from "@angular/material/expansion";
+import { MatGridListModule } from "@angular/material/grid-list";
+import { MatIconModule } from "@angular/material/icon";
+import { MatInputModule } from "@angular/material/input";
+import { MatListModule } from "@angular/material/list";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatNativeDateModule, MatRippleModule } from "@angular/material/core";
+import { MatPaginatorModule } from "@angular/material/paginator";
+import { MatProgressBarModule } from "@angular/material/progress-bar";
+import { MatProgressSpinnerModule } from "@angular/material/progress-spinner";
+import { MatRadioModule } from "@angular/material/radio";
+import { MatSelectModule } from "@angular/material/select";
+import { MatSidenavModule } from "@angular/material/sidenav";
+import { MatSliderModule } from "@angular/material/slider";
+import { MatSlideToggleModule } from "@angular/material/slide-toggle";
+import { MatSnackBarModule } from "@angular/material/snack-bar";
+import { MatSortModule } from "@angular/material/sort";
+import { MatTableModule } from "@angular/material/table";
+import { MatTabsModule } from "@angular/material/tabs";
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatTooltipModule } from "@angular/material/tooltip";
+import { MatTreeModule } from "@angular/material/tree";
+
+import { NgModule } from "@angular/core";
 
 @NgModule({
   imports: [
     MatAutocompleteModule,
     MatButtonModule,
+    MatBottomSheetModule,
     MatButtonToggleModule,
     MatCardModule,
     MatCheckboxModule,
@@ -70,11 +77,14 @@ import { NgModule } from '@angular/core';
     MatTabsModule,
     MatToolbarModule,
     MatTooltipModule,
-    CdkTableModule
+    CdkTableModule,
+    MatTreeModule,
+    MatBadgeModule
   ],
   exports: [
     MatAutocompleteModule,
     MatButtonModule,
+    MatBottomSheetModule,
     MatButtonToggleModule,
     MatCardModule,
     MatCheckboxModule,
@@ -104,7 +114,10 @@ import { NgModule } from '@angular/core';
     MatTabsModule,
     MatToolbarModule,
     MatTooltipModule,
-    CdkTableModule
-  ]
+    CdkTableModule,
+    MatTreeModule,
+    MatBadgeModule
+  ],
+  providers: []
 })
 export class MaterialModule {}
